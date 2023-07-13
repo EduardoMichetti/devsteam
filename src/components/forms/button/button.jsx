123b5cf425fc38577d5c12812c5041e9235df093
Eduardo Michetti <94409995+EduardoMichetti@users.noreply.github.com>
2023-07-13 19:44:23 -0300

    criado gamecard e alterado prop botao

--- src/components/forms/button/button.jsx
+++ src/components/forms/button/button.jsx
@@ -1,7 +1,7 @@
 import styles from './button.module.css'
 
-export default function Button ({ children }) {
+export default function Button ({ children, fullWidh }) {
     return (
-        <button className={styles.button} > {children} </button>
+        <button className={`${styles.button} ${fullWidh && styles.fullwidth}`} > {children} </button>
     )
 }
